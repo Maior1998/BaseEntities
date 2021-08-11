578c3db148bcf3bb339dc4d4b03494e83e8d843c
Sergey Patoka <sergey.pat98@gmail.com>
2021-08-11 17:27:57 +0500

    Добавлен абстрактный класс с репозиторем EF для работы с OData

---
+++ BaseEntities/EFOdatatityRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseEntities
+{
+    public abstract class EFOdatatityRepository<TentityType, TContextType> : IOdataEntityRepository<TentityType> where TentityType : class, IBaseEntity where TContextType : DbContext
+    {
+        protected abstract Expression<Func<TContextType, DbSet<TentityType>>> EntityDbSetSelector { get; }
+        protected abstract TContextType ContextType { get; }
+        private Func<TContextType, DbSet<TentityType>> entityDbSetFunc;
+        private Func<TContextType, DbSet<TentityType>> EntityDbSetFunc => entityDbSetFunc ??= EntityDbSetSelector.Compile();
+
+        public async Task AddEntity(TentityType entity)
+        {
+            TContextType contextType = ContextType;
+            await EntityDbSetFunc(contextType).AddAsync(entity);
+            await contextType.SaveChangesAsync();
+        }
+
+        public async Task DeleteEntity(Guid id)
+        {
+            TContextType contextType = ContextType;
+            var entityDbSet = EntityDbSetFunc(contextType);
+            var entity = await entityDbSet.SingleAsync(x => x.Id == id);
+            entityDbSet.Remove(entity);
+            await contextType.SaveChangesAsync();
+        }
+
+        public IQueryable<TentityType> GetEntities(Expression<Func<TentityType, bool>> predicate)
+        {
+            TContextType contextType = ContextType;
+            var entityDbSet = EntityDbSetFunc(contextType);
+            return entityDbSet.Where(predicate);
+        }
+
+        public async Task UpdateEntity(TentityType entity)
+        {
+            TContextType contextType = ContextType;
+            contextType.Attach(entity);
+            await contextType.SaveChangesAsync();
+        }
+    }
+}
--- BaseEntities/IOdataEntityRepository.cs
+++ BaseEntities/IOdataEntityRepository.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.OData.Formatter;
 using Microsoft.AspNetCore.OData.Query;
 using Microsoft.AspNetCore.OData.Results;
 using Microsoft.AspNetCore.OData.Routing.Controllers;
+using Microsoft.EntityFrameworkCore;
 
 using System;
 using System.Collections.Generic;
@@ -12,15 +13,12 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
-using System.Web.Http.ModelBinding;
 
 namespace BaseEntities
 {
     public interface IOdataEntityRepository<T> where T : class, IBaseEntity
     {
         public IQueryable<T> GetEntities(Expression<Func<T, bool>> predicate);
-        public IQueryable<T> GetSingleEntity(Expression<Func<T, bool>> predicate);
-        public Task<T> GetEntityById(Guid id);
         public Task UpdateEntity(T entity);
         public Task AddEntity(T entity);
         public Task DeleteEntity(Guid id);
@@ -38,79 +36,11 @@ namespace BaseEntities
         {
             return repository.GetEntities(x => x.Id == id);
         }
-    }
-
-    public abstract class HelperODataController<T> : ODataController where T : class, IBaseEntity
-    {
-        public IOdataEntityRepository<T> repository { get; }
-        private IHttpContextAccessor httpContextAccessor { get; set; }
-        protected HelperODataController(
-            IOdataEntityRepository<T> repository,
-            IHttpContextAccessor httpContextAccessor) : base()
+        public static async Task<T?> GetEntityById<T>(this IOdataEntityRepository<T> repository, Guid id) where T : class, IBaseEntity
         {
-            this.repository = repository;
-            this.httpContextAccessor = httpContextAccessor;
-        }
-
-        [EnableQuery]
-        public IQueryable<T> Get()
-        {
-            return repository.GetAllEntities();
-        }
-
-        [EnableQuery]
-        public SingleResult<T> Get(Guid key)
-        {
-            return SingleResult.Create(repository.GetEntities(x => x.Id == key));
-        }
-
-        public async Task<IActionResult> Post(T entity)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-            await repository.AddEntity(entity);
-            return Created(entity);
-        }
-
-        public async Task<IActionResult> Patch([FromODataUri] Guid key, Delta<T> product)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            var entity = await repository.GetEntityById(key);
-            if (entity == null)
-            {
-                return NotFound();
-            }
-            product.Patch(entity);
-            await repository.UpdateEntity(entity);
-            return Updated(entity);
-        }
-
-        public async Task<IActionResult> Put([FromODataUri] Guid key, T update)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            await repository.UpdateEntity(update);
-            return Updated(update);
-        }
-
-        public async Task<IActionResult> Delete([FromODataUri] Guid key)
-        {
-            var product = await repository.GetEntityById(key);
-            if (product == null)
-            {
-                return NotFound();
-            }
-            await repository.DeleteEntity(key);
-            return NoContent();
+            return await repository.GetEntities(x => x.Id == id).SingleOrDefaultAsync();
         }
     }
+
+
 }
